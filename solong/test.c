b6b5a6c265e4ed77b943e6aab27c5d7932ea0f8a
Mohamed Abdessmad <mabdessm@c2r1p1.42nice.fr>
2024-08-22 15:30:53 +0200

    norm fix

--- solong/test.c
+++ solong/test.c
@@ -6,20 +6,20 @@
 /*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:52:23 by mabdessm          #+#    #+#             */
-/*   Updated: 2024/08/22 15:26:53 by mabdessm         ###   ########.fr       */
+/*   Updated: 2024/08/22 15:30:42 by mabdessm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
- 
+
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 	int		steps;
-} t_data;
- 
-int on_destroy(t_data *data)
+}	t_data;
+
+int	on_destroy(t_data *data)
 {
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 	mlx_destroy_display(data->mlx_ptr);
@@ -27,8 +27,8 @@ int on_destroy(t_data *data)
 	exit(0);
 	return (0);
 }
- 
-int on_keypress(int keysym, t_data *data)
+
+int	on_keypress(int keysym, t_data *data)
 {
 	if (keysym == 65307)
 		on_destroy(data);
@@ -40,10 +40,10 @@ int on_keypress(int keysym, t_data *data)
 	}
 	return (0);
 }
- 
-int main(void)
+
+int	main(void)
 {
-	t_data data;
+	t_data	data;
 
 	data.steps = 0;
 	data.mlx_ptr = mlx_init();
@@ -52,16 +52,16 @@ int main(void)
 	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "so_long");
 	if (!data.win_ptr)
 		return (free(data.mlx_ptr), 1);
- 
+
 	// Register key release hook
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
- 
+
 	// Register destroy hook
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
- 
+
 	// Generate Images
 	//mlx_loop_hook(data.mlx_ptr, /*function that creates the images, background and animations (draws map)*/, &data);
-	
+
 	// Loop over the MLX pointer
 	mlx_loop(data.mlx_ptr);
 	return (0);
